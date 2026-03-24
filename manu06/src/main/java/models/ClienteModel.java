f311426622e590eb5b18f8c3801232f0d1e0afcf
Emanuelly <manu.presteslopes10@gmail.com>
2026-03-24 12:45:54 -0300

    model

---
+++ manu06/src/main/java/models/ClienteModel.java
@@ -0,0 +1,51 @@
+package models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_cliente")
+public class ClienteModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name")
+    private String nome;
+    private String email;
+    @Column(name = "numero")
+    private String telefone;
+
+    public ClienteModel() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
